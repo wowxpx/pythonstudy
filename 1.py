c95add47ec7e2925f286df2ca5a488004f7474b2
wowxpx <76871898+wowxpx@users.noreply.github.com>
2021-01-05 01:33:08 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+print("---------芭茅溪传媒---------")
+temp = input("想要知道wifi密码吗？那你觉得幸运数字是几：")
+guess = int(temp)
+if guess == 8:
+    print("你居然猜中了幸运数字，wifi密码是：bamaoxi110")
+    print("芭茅溪欢迎您")
+else:
+    print("猜错了，幸运数字是8！")
+print("游戏结束，再会")
+
