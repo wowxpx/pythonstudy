4773ad22a54f2ea4b300adb9420bd0db08495a12
wowxpx <76871898+wowxpx@users.noreply.github.com>
2021-01-08 16:43:33 +0800

    Add files via upload

---
+++ 演草纸.py
@@ -0,0 +1,8 @@
+"""xpx的练习题"""
+fenshu = input("请告诉我你这次数学考试的成绩：")
+daan = int(fenshu)
+if daan ==100:
+      print("不错，你离女神又近了一步。")
+
+else:
+      print("小子，想要幸福，就得努力。")
---
+++ 电脑算肖.py
@@ -0,0 +1,33 @@
+import random
+a = random.randint(1,49)
+
+
+
+print("猪小姐祝您发财！")
+print (a)
+if     a in [1,13,25,37,49] :
+    print("鼠")
+elif   a in [2,14,26,38] :
+    print("猪")
+elif   a in [3,15,27,39] :
+    print("狗")
+elif   a in [4,16,28,40] :
+    print("鸡")
+elif   a in [5,17,29,41] :
+    print("狗")
+elif   a in [6,18,30,42] :
+    print("羊")
+elif   a in [7,19,31,43] :
+    print("马")
+elif   a in [8,20,32,44] :
+    print("蛇")
+elif   a in [9,21,33,45] :
+    print("龙")
+elif   a in [10,22,34,46] :
+    print("猴")
+elif   a in [11,23,35,47] :
+    print("虎")
+elif   a in [12,24,36,48] :
+    print("牛")
+print("猪小姐祝您发财！")
+
